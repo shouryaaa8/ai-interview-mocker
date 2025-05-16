d210a9b880c8de67c2112c36e898e21a884adf05
shouryaaa8 <145766969+shouryaaa8@users.noreply.github.com>
2025-05-16 22:52:05 +0530

    Update Header.jsx

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
     })
   return (
     <div className='flex px-4 items-center justify-between bg-secondary shadow-sm'>
-      <img src="https://assets.grok.com/users/6f79ec4b-b50b-4c97-aeb8-3d1102be3730/generated/oi7MpPprayD5Vwlb/image.jpg" alt="logo" width={100} height={100} className='h-fit w-36' />
+      <img src="https://assets.grok.com/users/6f79ec4b-b50b-4c97-aeb8-3d1102be3730/generated/J4LmgWE7gStmwbRh/image.jpg" alt="logo" width={100} height={100} className='h-fit w-36' />
       <ul className=' hidden md:flex gap-6'>
         <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path === '/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
         <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path === '/dashboard/questions' && 'text-primary font-bold'}`}>Questions</li>
