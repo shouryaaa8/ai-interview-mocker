75f1d5234b332ae2a802f21928efc032d5a2fb2e
shouryaaa8 <145766969+shouryaaa8@users.noreply.github.com>
2025-05-16 22:50:24 +0530

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -28,12 +28,11 @@ export default function Page() {
           </a>
 
           <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-          Welcome to A Interview Mocker 🦑
+          Welcome to AI Interview Mocker 🦑
           </h2>
 
           <p className="mt-4 leading-relaxed text-white/90">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-            nam dolorum aliquam, quibusdam aperiam voluptatum.
+            Sharpen your interview skills with AI-powered mock interviews tailored to your dream job — anytime, anywhere.
           </p>
         </div>
       </section>
